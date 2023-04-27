d45fb47fd21dac238300d6c7ad5a98dc35455536
Aleksander <guree.19.05.94@gmail.com>
2023-04-27 17:16:07 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import { clearMarking, searchCountry } from './function'
+const _ = require('lodash');
 
+const refs = {
+    input: document.querySelector('#search-box'),
+    counrylist: document.querySelector('.country-list'),
+    countryInf: document.querySelector('.country-info'),
+};
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener("input", _.debounce(fetchCountries, DEBOUNCE_DELAY));
+
+function fetchCountries() {
+    clearMarking(refs);
+    searchCountry(refs);
+}
